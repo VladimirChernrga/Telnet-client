da1bc3df200d422573b1d9d5c340664f29f3de8e
Vladimir Chernega <vchernega@renrns.local>
2018-05-02 15:03:18 -0400

    Add project files.

---
+++ Telnet/Main.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Telnet
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(142, 762);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(777, 20);
+            this.textBoxSend.TabIndex = 1;
+            this.textBoxSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSend_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 762);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Send";
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Location = new System.Drawing.Point(32, 187);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.Size = new System.Drawing.Size(887, 552);
+            this.richTextBoxOutput.TabIndex = 4;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 830);
+            this.Controls.Add(this.richTextBoxOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSend);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+    }
+}
+
---
+++ Telnet/Main.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace Telnet
+{
+
+
+    public partial class Main : Form
+    {
+        Telnet tc = new Telnet();
+
+        public Main()
+        {
+            InitializeComponent();
+            //textBoxOutput.ScrollBars = ScrollBars.Both;
+            //textBoxOutput.WordWrap = true;
+            //textBoxOutput.ReadOnly = true;
+            richTextBoxOutput.ReadOnly = true;
+            
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //create a new telnet connection to hostname "gobelijn" on port "23"
+            
+            tc.TelnetConnection("192.168.25.245", 23);
+
+            textBoxSend.Focus();
+
+            //login with user "root",password "rootpassword", using a timeout of 100ms, and show server output
+            //string s = tc.Login("root", "rootpassword",100);
+            //Console.Write(s);
+
+            // server output should end with "$" or ">", otherwise the connection failed
+            //string prompt = s.TrimEnd();
+            //prompt = s.Substring(prompt.Length -1,1);
+            //if (prompt != "$" && prompt != ">" )
+            //  throw new Exception("Connection failed");
+
+            //prompt = "";
+
+            // while connected
+
+
+            string prompt = "";
+            int count = 0;
+            while (tc.IsConnected && count < 1)
+            {
+
+                System.Threading.Thread.Sleep(1500);
+
+                richTextBoxOutput.Text = tc.Read() + Environment.NewLine;
+                //Console.Write(tc.Read());
+
+                count++;
+
+            }
+            //Console.Write("!root");
+            //SendKeys.Send("{ENTER}");
+
+            /*
+            while (tc.IsConnected)
+            {
+                // display server output
+                //string console = tc.Read();
+
+
+
+                // send client input to server
+                prompt = Console.ReadLine();
+                tc.WriteLine(prompt);
+
+                // display server output
+                Console.Write(tc.Read());
+
+            }
+            Console.WriteLine("***DISCONNECTED");
+            Console.ReadLine();
+            */
+        }
+
+       
+
+        private void output()
+        {
+            
+        }
+
+        private void textBoxSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                DateTime localDate = DateTime.Now;
+               
+                richTextBoxOutput.Text += textBoxSend.Text+ Environment.NewLine + localDate.ToString() + Environment.NewLine;
+                
+                
+                tc.WriteLine(textBoxSend.Text);
+
+
+                richTextBoxOutput.Text += tc.Read() + Environment.NewLine;
+
+                textBoxSend.Clear();
+            }
+        }
+    }
+}
